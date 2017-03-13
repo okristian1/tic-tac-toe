22156788ef6e5ef61fd222bea3900b4941faf996
locrin <okristian1@gmail.com>
2017-03-13 15:44:39 +0100

    board html and css some pseudo code

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+var tiles = document.querySelectorAll('.board span');
+
+
+
+
+/* game logics
+Chose number of players
+Choose X or O
+Random X or O begins
+When tile is clicked set tile to current player
+X or O
+Unless tile already ocupied
+Check win condition and declare winner or change
+active player. repeat.
+ 
+
+*/
